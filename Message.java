94fbd565aa401da46b12d45cb5fc118b6652e202
applecider231 <stuart.savannah916@gmail.com>
2014-11-13 11:50:04 -0500

    Message and User files
    my first files that no one could see, kinda similar to Campbell's at the
    moment

---
+++ Message.java
@@ -0,0 +1,19 @@
+public class Message{
+	public Message(User user, String new_message){
+
+		if(loggedOn){
+
+		}else{
+
+		}
+
+
+	}
+
+///////////////adding messages to the file/////////////////////////////////
+//writer.write("message no. " + numMessages + "posted on " date + ":");
+//writer.write(message);
+//numMessages++;
+
+	String message;
+}
---
+++ User.java
@@ -0,0 +1,66 @@
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class User{
+
+	public User(new_username, new_password){ //constructor that creates a user with a username and password
+		username = new_username;
+		password = new_password;
+
+		//add to an arraylist of usernames to access later
+		
+		boolean added = false;
+		
+		while(!added){
+			if(!ListOfUsernames.contains(username)){ //an easy way to check if a username has been taken yet
+
+				ListOfUsernames.add(username);
+
+				//creating the text file for the user data
+				String userDataFile = username + ".txt";
+				File newTextFile = new File(userDataFile);
+
+				//enables writing to the created text file
+				try{
+					BufferedWriter writer = 
+							new BufferedWriter(new FileWriter(newTextFIle));
+					writer.write("Username: " + username);
+					writer.write("Password: " + password);
+					writer.close();
+					added = true;
+				}catch(IOException e){
+					e.printStackTrace();
+				}
+			}else{
+				System.out.println("This username is already taken, please enter another one.");
+			}
+		}
+	}
+
+	public void setPassword(new_password){ //we would first need to make sure the user is logged in
+		password = new_password;
+	}
+
+/* THIS MIGHT NEED TO GO IN THE MAIN PROGRAM
+
+	public boolean verifyUser(entered_username, entered_password){
+
+		if((entered_username.equals(username))&&(entered_password))
+
+	} 
+*/
+
+
+	protected String username;
+	protected String password;
+	package boolean loggedOn = false; //this just saves us the hassle of using getters and setters
+										//to check if the user is logged in since 
+										//this value can be changed in the main program
+
+	protected List<String> ListOfUsernames = new ArrayList<String>();
+	//somehow we need to make sure this data doesn't get deleted at the end of runtime
+	//maybe we could have it read from a file that has a list of the usernames?
+}
