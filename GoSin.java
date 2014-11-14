19d6a40823a34d92bf42a7c35c0b816527a0c98c
applecider231 <stuart.savannah916@gmail.com>
2014-11-14 15:34:47 -0500

    added main function (silly me forgot to add that earlier)

--- GoSin.java
+++ GoSin.java
@@ -2,28 +2,32 @@ import java.util.Scanner;
 
 public class GoSin{
 
-	boolean loggedOn = false; 
-	Scanner in = new Scanner(System.in);
-	String menu_input = "";
+	public static void main(String[] args) {
+		
+	
+		boolean loggedOn = false; 
+		Scanner in = new Scanner(System.in);
+		String menu_input = "";
 
-	do{
-		System.out.println("Welcome to GoSin, enter one of these options to get started!");
-		System.out.println("1: Register/Log in");
-		System.out.println("2: Post message");
-		System.out.println("3: Browse messages");
-		System.out.println("Q: Quit");
-		menu_input = in.nextLine();
+		do{
+			System.out.println("Welcome to GoSin, enter one of these options to get started!");
+			System.out.println("1: Register/Log in");
+			System.out.println("2: Post message");
+			System.out.println("3: Browse messages");
+			System.out.println("Q: Quit");
+			menu_input = in.nextLine();
 
-	}while((!loggedOn)&&(!menu_input.equals("Q"))&&(!menu_input.equals("q")));
-	//menu runs until the user quits or the user logs in
+		}while((!loggedOn)&&(!menu_input.equals("Q"))&&(!menu_input.equals("q")));
+		//menu runs until the user quits or the user logs in
 
-	if(menu_input.equals("1")){
+		if(menu_input.equals("1")){
 
-	}
+		}
 
 
-	if(menu_input.equals("Q")||menu_input.equals("q")){
-		System.out.println("Have a nice day!");
+		if(menu_input.equals("Q")||menu_input.equals("q")){
+			System.out.println("Have a nice day!");
+		}
 	}
 	
 }
