73b1ab069eee0bdfd2bacb384857edc1ba6b6504
applecider231 <stuart.savannah916@gmail.com>
2014-11-14 16:18:50 -0500

    working on implementing other menu options

--- GoSin.java
+++ GoSin.java
@@ -19,23 +19,34 @@ public class GoSin{
 			System.out.println("Q: Quit");
 			menu_input = in.nextLine();
 
+		
+
+			if(menu_input.equals("1")){
+				System.out.println("Please enter an original username, be creative! ");
+				String new_username = in.nextLine();
+				//check if name is taken
+
+				System.out.println("Please enter a password, don't forget it! ");
+				String new_password = in.nextLine();
+				currentUser = new User(new_username, new_password);
+				currentUser.setLoggedOnTrue();
+				System.out.println("Congratulations! You now have an account with GoSin. GO FORTH AND FUCK SHIT UP.");
+
+			}
+
+			else if(menu_input.equals("2")){
+
+			}
+			else if(menu_input.equals("3")){
+				
+			}
+			else if(menu_input.equals("4")){
+				
+			}
+			else if(menu_input.equals("Q")||menu_input.equals("q")){
+				System.out.println("Have a nice day!");
+			}
 		}while((!loggedOn)&&(!menu_input.equals("Q"))&&(!menu_input.equals("q")));
 		//menu runs until the user quits or the user logs in
-
-		if(menu_input.equals("1")){
-			System.out.println("Please enter an original username, be creative! ");
-			String new_username = in.nextLine();
-			System.out.println("Please enter a password, don't forget it! ");
-			String new_password = in.nextLine();
-			currentUser = new User(new_username, new_password);
-			System.out.println("Congratulations! You now have an account with GoSin. GO FORTH AND FUCK SHIT UP.");
-
-		}
-
-
-		if(menu_input.equals("Q")||menu_input.equals("q")){
-			System.out.println("Have a nice day!");
-		}
-	}
-	
+	}	
 }
