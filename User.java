aa381bb6644bd2f9e551c0551dfe48c1a0455d3d
applecider231 <stuart.savannah916@gmail.com>
2014-11-14 15:59:45 -0500

    changed java.util.ArrayList to java.util.*

--- User.java
+++ User.java
@@ -2,7 +2,7 @@ import java.io.File;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.*;
 
 public class User{
 
