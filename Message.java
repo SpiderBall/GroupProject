553bcf9d04317a6977d1c54078dc2779df5fdb95
applecider231 <stuart.savannah916@gmail.com>
2014-11-14 15:24:56 -0500

    added Campbells methods to the message class

--- Message.java
+++ Message.java
@@ -1,5 +1,34 @@
 public class Message{
-	public Message(User user, String new_message){
+
+//the private message class will inherit from this class
+//and it will add a data member "reciptient"
+//which is the username of the person it was sent to
+//and when an account logs in only those PMs will be loaded
+
+//this is the class that our "tweets" will use
+
+   Message(String inContent, String inUser){
+      messageContent = inContent;
+      userPosted = inUser;
+      
+      //down here we store the date and time that the message was created
+      //but I haven't done that yet
+   }
+   
+   public void deleteMessage(){
+      Scanner in = new Scanner(System.in);
+      System.out.println("Why is this message being removed?");
+      String reason = in.nextLine();
+      deleteMessageBackend(reason);
+      System.out.println("Message removed.");
+   }
+   
+   public void deleteMessageBackend(String reason){
+      messageContent = ("Message has been removed. Reason given: '"+reason+"'.");
+   }
+   
+/*
+   public Message(User user, String new_message){
 
 		if(loggedOn){
 
@@ -15,5 +44,12 @@ public class Message{
 //writer.write(message);
 //numMessages++;
 
-	String message;
+	String message; */
+
+
+
+   //DATA MEMBERS
+   protected String messageContent;
+   protected String userPosted;
+
 }
